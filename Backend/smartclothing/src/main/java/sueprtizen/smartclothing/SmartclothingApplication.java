a55bb4c371d19521014248cbb62c8138c75145b7
김지현 <kjihyun0331@naver.com>
2024-03-13 17:36:25 +0900

    [BE] ADD : initialize spring boot project

---
+++ Backend/smartclothing/src/main/java/sueprtizen/smartclothing/SmartclothingApplication.java
@@ -0,0 +1,13 @@
+package sueprtizen.smartclothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartclothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartclothingApplication.class, args);
+	}
+
+}
