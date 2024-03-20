81c824f9c56c2d64ae32f3e64dd2434de20ee2f0
황성재 <woakd0705@naver.com>
2024-03-20 18:02:24 +0900

    [TIZEN] UPDATE : add button callback, remove button background

--- TIZEN/Washer/src/washer.c
+++ TIZEN/Washer/src/washer.c
@@ -20,6 +20,29 @@ win_back_cb(void *data, Evas_Object *obj, void *event_info)
 	elm_win_lower(ad->win);
 }
 
+struct temp {
+	Evas_Object *nf;
+	char *edj_path;
+	Evas_Object *laundry_layout;
+	Evas_Object *course_box;
+} obj_temp;
+
+void clicked_cb(void *data, Evas_Object *obj, void *event_info) {
+	/*
+	 *	laundry expand view
+	 */
+	obj_temp.laundry_layout = elm_layout_add(obj_temp.nf);
+	elm_layout_file_set(obj_temp.laundry_layout, obj_temp.edj_path, "main_layout");
+	elm_naviframe_item_push(obj_temp.nf, "laundry", NULL, NULL, obj_temp.laundry_layout, NULL);
+	evas_object_size_hint_align_set(obj_temp.laundry_layout, 0.5, 0.5);
+
+	obj_temp.course_box = elm_layout_add(obj_temp.laundry_layout);
+	elm_layout_file_set(obj_temp.course_box, obj_temp.edj_path, "box_wrapper");
+	evas_object_size_hint_min_set(obj_temp.course_box, 750, 750);
+	elm_layout_content_set(obj_temp.laundry_layout, "elm.swallow.content", obj_temp.course_box);
+	evas_object_show(obj_temp.course_box);
+}
+
 static void
 create_base_gui(appdata_s *ad)
 {
@@ -77,25 +100,132 @@ create_base_gui(appdata_s *ad)
 //	    free(img_path);
 //	}
 
-	Evas_Object *nf = elm_naviframe_add(ad->conform);
-	elm_object_content_set(ad->conform, nf);
-	evas_object_show(nf);
-
 	char *edj_path = NULL;
 	app_resource_manager_get(APP_RESOURCE_TYPE_LAYOUT, "edje_res/sample.edj", &edj_path);
+	elm_theme_extension_add(NULL, edj_path);
 
 	char *expand_path = NULL;
 	app_resource_manager_get(APP_RESOURCE_TYPE_IMAGE, "expand.png", &expand_path);
 
+	Evas_Object *nf = elm_naviframe_add(ad->conform);
+	elm_object_content_set(ad->conform, nf);
+	evas_object_show(nf);
+
+	obj_temp.nf = nf;
+	obj_temp.edj_path = edj_path;
+
+	/*
+	 *	main view
+	 */
 	Evas_Object *main_layout = elm_layout_add(nf);
 	elm_layout_file_set(main_layout, edj_path, "main_layout");
-	elm_naviframe_item_push(nf, "Layout", NULL, NULL, main_layout, NULL);
+	elm_naviframe_item_push(nf, NULL, NULL, NULL, main_layout, NULL);
 	elm_box_align_set(main_layout, 0.5, 0.5);
 
-	Evas_Object *laundry_box_wrapper = elm_layout_add(main_layout);
+	Evas_Object *menu_scroller = elm_scroller_add(main_layout);
+	elm_object_style_set(menu_scroller, "menu_scroller");
+	elm_layout_content_set(main_layout, "elm.swallow.content", menu_scroller);
+	evas_object_show(menu_scroller);
+
+	Evas_Object *menu_box = elm_box_add(menu_scroller);
+	elm_box_horizontal_set(menu_box, EINA_TRUE);
+	elm_box_padding_set(menu_box, 100, 0);
+	elm_layout_content_set(menu_scroller, "elm.swallow.content", menu_box);
+	evas_object_show(menu_box);
+
+	/*
+	 * 	course box
+	 */
+	Evas_Object *course_box_wrapper = elm_layout_add(menu_box);
+	elm_layout_file_set(course_box_wrapper, edj_path, "box_wrapper");
+	evas_object_size_hint_min_set(course_box_wrapper, 750, 750);
+	elm_box_pack_end(menu_box, course_box_wrapper);
+	evas_object_show(course_box_wrapper);
+
+	Evas_Object *course_box = elm_box_add(course_box_wrapper);
+	elm_layout_content_set(course_box_wrapper, "elm.swallow.content", course_box);
+	elm_box_padding_set(course_box, 0, 25);
+	evas_object_show(course_box);
+
+	Evas_Object *course_menu_box = elm_box_add(course_box);
+	evas_object_size_hint_align_set(course_menu_box, EVAS_HINT_FILL, 0);
+	elm_box_horizontal_set(course_menu_box, EINA_TRUE);
+	elm_box_pack_end(course_box, course_menu_box);
+	evas_object_show(course_menu_box);
+
+	Evas_Object *course_menu_label = elm_label_add(course_menu_box);
+	elm_object_text_set(course_menu_label, "<color=#ABABABFF font_size=55 font_weight=BOLD>코스</color>");
+	elm_box_pack_end(course_menu_box, course_menu_label);
+	evas_object_show(course_menu_label);
+
+	Evas_Object *course_menu_empty = elm_layout_add(course_menu_box);
+	evas_object_size_hint_weight_set(course_menu_empty, EVAS_HINT_EXPAND, 0);
+	elm_box_pack_end(course_menu_box, course_menu_empty);
+	evas_object_show(course_menu_empty);
+
+	Evas_Object *course_expand_btn = elm_button_add(course_menu_box);
+	elm_object_style_set(course_expand_btn, "expand_button");
+	evas_object_size_hint_min_set(course_expand_btn, 80, 80);
+	Evas_Object *course_expand_btn_img = elm_image_add(course_expand_btn);
+	elm_image_file_set(course_expand_btn_img, expand_path, NULL);
+	elm_object_part_content_set(course_expand_btn, "icon", course_expand_btn_img);
+	elm_box_pack_end(course_menu_box, course_expand_btn);
+	evas_object_show(course_expand_btn);
+
+	Evas_Object *course_list_box = elm_box_add(course_box);
+	elm_box_horizontal_set(course_list_box, EINA_TRUE);
+	elm_box_padding_set(course_list_box, 50, 0);
+	elm_box_pack_end(course_box, course_list_box);
+	evas_object_show(course_list_box);
+
+	char course_img_names[2][100] = { "course_ai.png", "course_standard.png" };
+	char course_names[2][100] = { "AI 맞춤", "표준" };
+	char course_css[100] = "<color=#CFCFCF font_size=45 font_weight=BOLD>%s</color>";
+
+	for(int i = 0; i < 2; i++){
+		Evas_Object *course_wrapper = elm_layout_add(course_list_box);
+		evas_object_size_hint_min_set(course_wrapper, 300, 600);
+		elm_layout_file_set(course_wrapper, edj_path, "course_wrapper");
+		elm_box_pack_end(course_list_box, course_wrapper);
+		evas_object_show(course_wrapper);
+
+		Evas_Object *course = elm_box_add(course_wrapper);
+		elm_box_align_set(course, 0.5, 0);
+		elm_box_padding_set(course, 0, 20);
+		elm_layout_content_set(course_wrapper, "elm.swallow.content", course);
+		evas_object_show(course);
+
+		Evas_Object *course_image_wrapper = elm_layout_add(course);
+		elm_layout_file_set(course_image_wrapper, edj_path, "course_image_wrapper");
+		elm_box_pack_end(course, course_image_wrapper);
+		evas_object_size_hint_align_set(course_image_wrapper, 0, 0.5);
+		evas_object_show(course_image_wrapper);
+
+		char *course_img_path = NULL;
+		app_resource_manager_get(APP_RESOURCE_TYPE_IMAGE, course_img_names[i], &course_img_path);
+		Evas_Object *course_image = elm_image_add(course_image_wrapper);
+		evas_object_size_hint_min_set(course_image, 60, 60);
+		evas_object_size_hint_max_set(course_image, 60, 60);
+		elm_image_file_set(course_image, course_img_path, NULL);
+		elm_layout_content_set(course_image_wrapper, "elm.swallow.content", course_image);
+		evas_object_show(course_image);
+
+		Evas_Object *course_label = elm_label_add(course);
+		evas_object_size_hint_align_set(course_label, 0, 0.5);
+		char content[200];
+		snprintf(content, 200, course_css, course_names[i]);
+		elm_object_text_set(course_label, content);
+		elm_box_pack_end(course, course_label);
+		evas_object_show(course_label);
+	}
+
+	/*
+	 *	laundry box
+	 */
+	Evas_Object *laundry_box_wrapper = elm_layout_add(menu_box);
 	elm_layout_file_set(laundry_box_wrapper, edj_path, "box_wrapper");
-	elm_layout_content_set(main_layout, "elm.swallow.content", laundry_box_wrapper);
-	evas_object_size_hint_max_set(laundry_box_wrapper, 0, 0);
+	evas_object_size_hint_min_set(laundry_box_wrapper, 750, 750);
+	elm_box_pack_end(menu_box, laundry_box_wrapper);
 	evas_object_show(laundry_box_wrapper);
 
 	Evas_Object *laundry_box = elm_box_add(laundry_box_wrapper);
@@ -103,33 +233,31 @@ create_base_gui(appdata_s *ad)
 	elm_box_padding_set(laundry_box, 0, 25);
 	evas_object_show(laundry_box);
 
-	Evas_Object *menu_box = elm_box_add(laundry_box);
-	evas_object_size_hint_align_set(menu_box, EVAS_HINT_FILL, 0);
-	elm_box_horizontal_set(menu_box, EINA_TRUE);
-	elm_box_pack_end(laundry_box, menu_box);
-	evas_object_show(menu_box);
+	Evas_Object *laundry_menu_box = elm_box_add(laundry_box);
+	evas_object_size_hint_align_set(laundry_menu_box, EVAS_HINT_FILL, 0);
+	elm_box_horizontal_set(laundry_menu_box, EINA_TRUE);
+	elm_box_pack_end(laundry_box, laundry_menu_box);
+	evas_object_show(laundry_menu_box);
 
-	Evas_Object *laundry_menu_label = elm_label_add(menu_box);
-	elm_object_text_set(laundry_menu_label, "<color=#ABABABFF font_size=50>세탁물 확인</color>");
-	elm_box_pack_end(menu_box, laundry_menu_label);
+	Evas_Object *laundry_menu_label = elm_label_add(laundry_menu_box);
+	elm_object_text_set(laundry_menu_label, "<color=#ABABABFF font_size=55 font_weight=BOLD>세탁물 확인</color>");
+	elm_box_pack_end(laundry_menu_box, laundry_menu_label);
 	evas_object_show(laundry_menu_label);
 
-	Evas_Object *laundry_menu_empty = elm_layout_add(menu_box);
+	Evas_Object *laundry_menu_empty = elm_layout_add(laundry_menu_box);
 	evas_object_size_hint_weight_set(laundry_menu_empty, EVAS_HINT_EXPAND, 0);
-	elm_box_pack_end(menu_box, laundry_menu_empty);
+	elm_box_pack_end(laundry_menu_box, laundry_menu_empty);
 	evas_object_show(laundry_menu_empty);
 
-	Evas_Object *expand_btn = elm_button_add(menu_box);
-	//elm_object_style_set(expand_btn, "editfield_clear");
-	elm_layout_file_set(expand_btn, edj_path, "expand_button");
-	Evas_Object *expand_btn_img = elm_image_add(expand_btn);
-	elm_image_file_set(expand_btn_img, expand_path, NULL);
-//	evas_object_size_hint_min_set(expand_btn_img, 100, 100);
-//	evas_object_size_hint_weight_set(expand_btn_img, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-//	evas_object_size_hint_align_set(expand_btn_img, EVAS_HINT_FILL, EVAS_HINT_FILL);
-	elm_object_part_content_set(expand_btn, "icon", expand_btn_img);
-	elm_box_pack_end(menu_box, expand_btn);
-	evas_object_show(expand_btn);
+	Evas_Object *laundry_expand_btn = elm_button_add(laundry_menu_box);
+	elm_object_style_set(laundry_expand_btn, "expand_button");
+	evas_object_size_hint_min_set(laundry_expand_btn, 80, 80);
+	Evas_Object *laundry_expand_btn_img = elm_image_add(laundry_expand_btn);
+	elm_image_file_set(laundry_expand_btn_img, expand_path, NULL);
+	elm_object_part_content_set(laundry_expand_btn, "icon", laundry_expand_btn_img);
+	elm_box_pack_end(laundry_menu_box, laundry_expand_btn);
+	evas_object_show(laundry_expand_btn);
+	evas_object_smart_callback_add(laundry_expand_btn, "clicked", clicked_cb, NULL);
 
 	Evas_Object *laundry_list_box = elm_box_add(laundry_box);
 	elm_box_horizontal_set(laundry_list_box, EINA_TRUE);
@@ -137,7 +265,7 @@ create_base_gui(appdata_s *ad)
 	elm_box_pack_end(laundry_box, laundry_list_box);
 	evas_object_show(laundry_list_box);
 
-	char details[2][3][2][100] = {
+	char laundry_details[2][3][2][100] = {
 			{
 					{"재질", "코튼"},
 					{"오염도", "2회 착용"},
@@ -149,7 +277,7 @@ create_base_gui(appdata_s *ad)
 					{"일정", "4/22"}
 			}
 	};
-	char detail_css[2][100] = {
+	char laundry_detail_css[2][100] = {
 			"<color=#A5A5A5FF font_size=27>%s</color>",
 			"<color=#EFEFEFFF font_size=32>%s</color>"
 	};
@@ -163,7 +291,7 @@ create_base_gui(appdata_s *ad)
 
 		Evas_Object *laundry = elm_box_add(laundry_wrapper);
 		elm_box_align_set(laundry, 0.5, 0);
-		elm_box_padding_set(laundry, 0, 20);
+		elm_box_padding_set(laundry, 0, 30);
 		elm_layout_content_set(laundry_wrapper, "elm.swallow.content", laundry);
 		evas_object_show(laundry);
 
@@ -172,31 +300,45 @@ create_base_gui(appdata_s *ad)
 		Evas_Object *laundry_img = elm_image_add(laundry);
 		evas_object_size_hint_min_set(laundry_img, 150, 230);
 		evas_object_size_hint_max_set(laundry_img, 150, 230);
-//		elm_image_no_scale_set(laundry_img, EINA_TRUE);
-//		elm_image_resizable_set(laundry_img, EINA_TRUE, EINA_TRUE);
 		elm_image_file_set(laundry_img, laundry_img_path, NULL);
 		elm_box_pack_end(laundry, laundry_img);
 		evas_object_show(laundry_img);
 
-		Evas_Object *laundry_detail = elm_table_add(laundry);
-		evas_object_size_hint_weight_set(laundry_detail, EVAS_HINT_EXPAND, 0);
-		evas_object_size_hint_align_set(laundry_detail, 0, 0);
-		elm_table_padding_set(laundry_detail, 15, 0);
-		elm_box_pack_end(laundry, laundry_detail);
-		evas_object_show(laundry_detail);
+		Evas_Object *laundry_detail_table = elm_table_add(laundry);
+		evas_object_size_hint_weight_set(laundry_detail_table, EVAS_HINT_EXPAND, 0);
+		evas_object_size_hint_align_set(laundry_detail_table, 0, 0);
+		elm_table_padding_set(laundry_detail_table, 15, 0);
+		elm_box_pack_end(laundry, laundry_detail_table);
+		evas_object_show(laundry_detail_table);
 
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 2; col++) {
-				Evas_Object *laundry_detail_label = elm_label_add(laundry_detail);
+				Evas_Object *laundry_detail_label = elm_label_add(laundry_detail_table);
 				evas_object_size_hint_align_set(laundry_detail_label, 0, 0.5);
 				char content[200];
-				snprintf(content, 200, detail_css[col], details[i][row][col]);
+				snprintf(content, 200, laundry_detail_css[col], laundry_details[i][row][col]);
 				elm_object_text_set(laundry_detail_label, content);
-				elm_table_pack(laundry_detail, laundry_detail_label, col, row, 1, 1);
+				elm_table_pack(laundry_detail_table, laundry_detail_label, col, row, 1, 1);
 				evas_object_show(laundry_detail_label);
 			}
 		}
 	}
+
+	/*
+	 *	notification(empty) box
+	 */
+	Evas_Object *notification_box_wrapper = elm_layout_add(menu_box);
+	elm_layout_file_set(notification_box_wrapper, edj_path, "box_wrapper");
+	evas_object_size_hint_min_set(notification_box_wrapper, 750, 750);
+	elm_box_pack_end(menu_box, notification_box_wrapper);
+	evas_object_show(notification_box_wrapper);
+
+	Evas_Object *notification_menu_label = elm_label_add(notification_box_wrapper);
+	elm_object_text_set(notification_menu_label, "<color=#ABABABFF font_size=55 font_weight=BOLD>알림</color>");
+	elm_layout_content_set(notification_box_wrapper, "elm.swallow.content", notification_menu_label);
+	evas_object_size_hint_align_set(notification_menu_label, 0.5, 0);
+	evas_object_show(notification_menu_label);
+
 
 	/* Show window after base gui is set up */
 	evas_object_show(ad->win);
