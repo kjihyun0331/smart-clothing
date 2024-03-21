28b56ab384ad00814a8ddc4233ed67cfbd797cc3
김승희 <libravickyyy@gmail.com>
2024-03-21 16:39:16 +0900

    [FE] feat : pwa-전환

--- FE/vite-project/src/main.tsx
+++ FE/vite-project/src/main.tsx
@@ -6,17 +6,16 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/theme";
 import { GlobalStyles } from "@/styles/reset";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+serviceWorkerRegistration.register();
+
 const queryClient = new QueryClient(); //2번
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <RouterProvider
-          router={router}
-          fallbackElement={<div>Loading...</div>}
-        />
-      </ThemeProvider>
-    </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={router} fallbackElement={<div>Loading...</div>} />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
