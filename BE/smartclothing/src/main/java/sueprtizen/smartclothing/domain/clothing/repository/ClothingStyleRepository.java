0e90e490fac1d9ceaebe5a1076093fb4fdfd78f7
이호성 <ghtjd8264@naver.com>
2024-03-27 02:03:39 +0900

    [BE] UPDATE : add repository annotations

--- BE/smartclothing/src/main/java/sueprtizen/smartclothing/domain/clothing/repository/ClothingStyleRepository.java
+++ BE/smartclothing/src/main/java/sueprtizen/smartclothing/domain/clothing/repository/ClothingStyleRepository.java
@@ -1,9 +1,11 @@
 package sueprtizen.smartclothing.domain.clothing.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sueprtizen.smartclothing.domain.clothing.entity.Clothing;
 import sueprtizen.smartclothing.domain.clothing.entity.ClothingStyle;
 
+@Repository
 public interface ClothingStyleRepository extends JpaRepository<ClothingStyle, Integer>{
 
     void deleteAllByClothing(Clothing clothing);
