9940c65b47c56585f6ca20df24dc97421e8cc0c6
김지현 <kjihyun0331@naver.com>
2024-03-28 01:10:52 +0900

    [BE] ADD : socket user api

--- BE/smartclothing/src/main/java/sueprtizen/smartclothing/socket/washer/service/WasherService.java
+++ BE/smartclothing/src/main/java/sueprtizen/smartclothing/socket/washer/service/WasherService.java
@@ -1,11 +1,12 @@
 package sueprtizen.smartclothing.socket.washer.service;
 
 import sueprtizen.smartclothing.domain.clothing.entity.Clothing;
+import sueprtizen.smartclothing.socket.washer.dto.WasherResponseDTO;
 
 import java.util.List;
 
 
 
 public interface WasherService {
-    List<Clothing> getAllLaundryList();
+    List<WasherResponseDTO> getAllLaundryList();
 }
--- BE/smartclothing/src/main/java/sueprtizen/smartclothing/socket/washer/service/WasherServiceImpl.java
+++ BE/smartclothing/src/main/java/sueprtizen/smartclothing/socket/washer/service/WasherServiceImpl.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sueprtizen.smartclothing.domain.clothing.entity.Clothing;
+import sueprtizen.smartclothing.socket.washer.dto.WasherResponseDTO;
 import sueprtizen.smartclothing.socket.washer.repository.WasherRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -17,7 +19,11 @@ public class WasherServiceImpl implements WasherService{
     private final WasherRepository washerRepository;
 
     @Transactional(readOnly = true)
-    public List<Clothing> getAllLaundryList(){
-        return washerRepository.findAllByNowAt("세탁기");
+    public List<WasherResponseDTO> getAllLaundryList(){
+        List<Clothing> laundry = washerRepository.findAllByNowAt("세탁기");
+        return laundry.stream().map(entity -> WasherResponseDTO.builder()
+                .worn_count(entity.getWornCount())
+                .build()
+        ).collect(Collectors.toList());
     }
 }
