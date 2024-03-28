c1bbe7c59967e17ea33aa58cc41a1dfcf7bd6698
김지현 <kjihyun0331@naver.com>
2024-03-28 17:28:22 +0900

    [BE] FIX : fcm fixed

--- BE/smartclothing/src/main/java/sueprtizen/smartclothing/global/fcm/FCMController.java
+++ BE/smartclothing/src/main/java/sueprtizen/smartclothing/global/fcm/FCMController.java
@@ -1,21 +1,19 @@
 package sueprtizen.smartclothing.global.fcm;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/notification")
+@RequestMapping("/notifications")
+@RestController
+@RequiredArgsConstructor
 public class FCMController {
 
     private final FCMService fcmService;
-
-    @Autowired
-    public FCMController(FCMService fcmService) {
-        this.fcmService = fcmService;
-    }
 
     @PostMapping("/")
     public ResponseEntity<String> sendNotification(@RequestBody @Valid FCMRequest request) {
