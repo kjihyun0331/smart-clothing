d1ab2b9a4cec3dcaabb988cbc00f8138e055b129
sanghoon <bsh4766@naver.com>
2024-04-01 16:05:15 +0900

    [FE] fix : userapi

--- FE/vite-project/src/hooks/useApi.ts
+++ FE/vite-project/src/hooks/useApi.ts
@@ -3,14 +3,13 @@ import { BASE_URL } from "@/config/config";
 import axios, { AxiosRequestConfig } from "axios";
 
 export function useApi(method: string, url: string, requestData?: string) {
-  // const userToken = localStorage.getItem("token");
+  const userToken = localStorage.getItem("token");
 
   const axiosRequestConfig: AxiosRequestConfig = {
     method: method,
     url: `${BASE_URL}/${url}`,
     headers: {
-      // "User-ID": userToken,
-      "User-ID": 2,
+      "User-ID": userToken,
     },
     ...(requestData && { data: requestData }),
   };
