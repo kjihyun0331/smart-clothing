395a681b836ea4320640aa2eb8968879ec881393
hhheeeeee <libravickyyy@gmail.com>
2024-04-01 18:51:40 +0900

    [FE] fix : 같이 입는 사람 본인 안뜨게 수정

--- FE/vite-project/src/sections/closet/SharedUsers.tsx
+++ FE/vite-project/src/sections/closet/SharedUsers.tsx
@@ -1,7 +1,7 @@
 import { useApi } from "@/hooks/useApi";
 
 function SharedUsers({ handleDispatch }) {
-  const { data, isLoading } = useApi("get", "users/family");
+  const { data, isLoading } = useApi("get", "users/family?includeSelf=false'");
 
   const handleClick = (item) => {
     // const value = event.target.textContent;
