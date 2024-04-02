3267a7856e8386a08e9d3ee1fe0b619a231de0a1
sanghoon <bsh4766@naver.com>
2024-04-02 21:25:38 +0900

    [ML] fix : bug fix

--- FE/vite-project/src/store/LocateStore.ts
+++ FE/vite-project/src/store/LocateStore.ts
@@ -12,5 +12,8 @@ export const useLocateStore = create<LocateState>((set) => ({
     Sido: '',
     Sigungu: '',
     LocateInfo: 0,
-    ChangeLocateInfo: ( newSido:string, newSigungu:string, newLocateInfo:number ) => set({ Sido: newSido, Sigungu: newSigungu, LocateInfo: newLocateInfo}),
+    ChangeLocateInfo: ( newSido:string, newSigungu:string, newLocateInfo:number ) => {
+      localStorage.setItem("locate", newLocateInfo.toString());
+      return set({ Sido: newSido, Sigungu: newSigungu, LocateInfo: newLocateInfo})
+    },
 }));
