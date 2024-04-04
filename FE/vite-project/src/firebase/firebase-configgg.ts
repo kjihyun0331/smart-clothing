a0f1378b06f3551ee14f4f84ac190eaeae98f2d1
김승희 <libravickyyy@gmail.com>
2024-04-04 10:40:38 +0900

    [FE] chore : api-key env로 분리

--- FE/vite-project/src/firebase/firebase-configgg.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyAX5dTw8B5y4lPSsHrhSwTyyrMLrib_rjA",
-  authDomain: "smartclothing-6d964.firebaseapp.com",
-  projectId: "smartclothing-6d964",
-  storageBucket: "smartclothing-6d964.appspot.com",
-  messagingSenderId: "75126210766",
-  appId: "1:75126210766:web:c1672e5eafaaea2736d09f",
-  measurementId: "G-CXPQ0KX5CN",
-};
